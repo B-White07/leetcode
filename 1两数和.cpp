434c2e3ba34e152039b7674bd1020efd3622eeaa
bwhite <1240160064@qq.com>
2024-06-11 21:47:13 +0800

    1

--- 1两数和.cpp
+++ 1两数和.cpp
@@ -1,28 +1,6 @@
-/*
+/**
 描述：
-给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
-你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
-你可以按任意顺序返回答案。
-
-示例 1：
-
-输入：nums = [2,7,11,15], target = 9
-输出：[0,1]
-解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
-示例 2：
-
-输入：nums = [3,2,4], target = 6
-输出：[1,2]
-示例 3：
-
-输入：nums = [3,3], target = 6
-输出：[0,1]
-
-
-get:
-vector.size()返回容器的长度
-
-rerurn {}的形式
+https://leetcode.cn/problems/two-sum/description/
 */
 
 
@@ -31,12 +9,12 @@ rerurn {}的形式
 
 using namespace std;
 
-//暴力解
+//暴力解n^2
 class Solution {
 public:
     vector<int> twoSum(vector<int> &nums, int target) {
         int i, j;
-        for (i = 0; i < nums.size() - 1; i++) {
+        for (i = 0; i < nums.size() - 1; i++) { //vector.size()返回容器的长度
             for (j = i + 1; j < nums.size(); j++) { //数组中同一个元素在答案里不能重复出现：就是自己不能和自己相加，因此j要从i的下一个位置开始
                 if (nums[i] + nums[j] == target) {
                     return {i,j};  //C++11新特性，将{}中的内容转换成相应的对象返回，如这里将i,j构造成vector对象返回
@@ -47,4 +25,28 @@ public:
     }
 };
 
-//哈希表
+//哈希表n
+/* begin()和end()都是迭代器，分别指向容器第一个位置和最后一个位置的下一个位置。迭代器可以暂时理解为指针
+ * m.first就是m的key,m.second就是vlaue
+ * map插入：可以用pair
+ *              也可以用map[key]=value的方式，map[key]=value也可以用于访问key的value
+ * map删除：m.erase(m.begin()) 删除第一个元素
+ *              m.erase(key)    按照key删除
+ *              m.erase(m.begin(),m.end())  按照区间删，这其实就是清空了，等价于m.clear()
+ * map查找：m.find(key)    若key存在，返回该key的迭代器；若不存在，返回m.end()
+ * map计数：m.count(key)   统计key的次数，只能是0或1，因为map不允许重复的key.mulmap允许重复
+ */
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> hash;   //unorder_map是无序的哈希表，map是有序的哈希表
+        for (int i = 0; i < nums.size(); i++) {
+            auto it = hash.find(target - nums[i]);
+            if (it == hash.end())
+                hash.insert(pair<int, int>(nums[i], i));    //insert插入需要用pair<>的方式插入键值对
+            else
+                return {it->second, i};
+        }
+        return {};
+    }
+};

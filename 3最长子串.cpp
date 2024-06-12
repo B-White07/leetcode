d21b8b0e1489606a158b2f1011ac054f670ae988
bwhite <1240160064@qq.com>
2024-06-12 22:59:16 +0800

    3

--- 3最长子串.cpp
+++ 3最长子串.cpp
@@ -18,22 +18,50 @@
  * multiset<>：可以查重复的值
  * pair<type,type> p    创建对组，p.first p.second 访问第一个和第二个元素
  */
+#include <iostream>;
+#include "map";
+
+using namespace std;
+
+//暴力解
+//class Solution {
+//public:
+//    int lengthOfLongestSubstring(string s) {
+//        set<char> str;  //装子串
+//        int max = 0;    //记录最大子串长度
+//
+//        for (int i = 0; i < s.size(); i++) {
+//            for (int j = i; j < s.size(); j++) {
+//                if (str.find(s[j]) == str.end())    //不是重复字符就加入子串
+//                    str.insert(s[j]);
+//                else
+//                    break;  //是重复字符，本趟结束，即找到了一个不重复的子串
+//            }
+//            str.size() > max ? max = str.size() : max;  //看看子串长度
+//            str.clear();    //清空以便保存下一个子串
+//        }
+//        return max;
+//    }
+//};
+
+//滑动窗口
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        set<char> str;  //装子串
-        int max = 0;    //记录最大子串长度
-
+        int max = 0;
+        unordered_map<char, int> um;
+        int left = 0;
         for (int i = 0; i < s.size(); i++) {
-            for (int j = i; j < s.size(); j++) {
-                if (str.find(s[j]) == str.end())    //不是重复字符就加入子串
-                    str.insert(s[j]);
-                else
-                    break;  //是重复字符，本趟结束，即找到了一个不重复的子串
+            if (um.find(s[i]) == um.end()) {
+                um.insert(pair<char, int>(s[i], i));
+            } else {
+                left = um[s[i]] + 1;
+                um.insert(pair<char, int>(s[i], i));
+                um.size() - left + 1 > max ? max = um.size() - left + 1 : max;
             }
-            str.size() > max ? max = str.size() : max;  //看看子串长度
-            str.clear();    //清空以便保存下一个子串
         }
+        um.size() - left + 1 > max ? max = um.size() - left + 1 : max;
         return max;
     }
 };
+

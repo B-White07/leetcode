55ac4570bc31416448aa370db3d40a26ee469495
bwhite <1240160064@qq.com>
2024-06-17 22:42:15 +0800

    update

--- 5最长回文串m.cpp
+++ 5最长回文串m.cpp
@@ -9,7 +9,7 @@
 using namespace std;
 
 //暴力解，效率太低了，过不了lc所有测试
-string longestPalindrome(string s) {
+/*string longestPalindrome(string s) {
     string s1 = "";
     string s2 = "";
     int index = 0, maxlen = 0;
@@ -37,7 +37,7 @@ string longestPalindrome(string s) {
     }
 
     return vec[index];
-}
+}*/
 
 //动态规划dp
 /*
@@ -48,7 +48,30 @@ string longestPalindrome(string s) {
  *      确定遍历顺序
  *      打印dp数组
  */
+string longestPalindrome(string s) {
+    vector<vector<bool> > dp(s.size(), vector<bool>(s.size(), false));  //初始化dp数组，默认所有串都不是回文串
+    int begin = 0;  //记录最长回文长起点
+    int maxlen = 0; //记录最长回文串长度
+
+    for (int i = s.size() - 1; i >= 0; i--) {
+        for (int j = i; j < s.size(); j++) {    //遍历dp数组，需要根据最下角的值来确定当前值，因此遍历顺序应该从最左下角开始，并且根据实际意义，j应该大于i.
+            if (s[i] == s[j]) { //当前字符相等，下面取决于两字符之间的是不是回文串
+                if (j - i < 3)  //长度小于3，一定是
+                    dp[i][j] = true;
+                else
+                    dp[i][j] = dp[i + 1][j - 1];    //中间是就是，中间不是我也不是
+            }
+
+            if (dp[i][j] && j - i + 1 > maxlen) {   //如果当前串是回文串，记录长度
+                begin = i;
+                maxlen = j - i + 1;
+            }
+        }
+    }
+    return s.substr(begin, maxlen);
+}
+
 int main() {
-    cout << longestPalindrome("ccc");
+    cout << longestPalindrome("babad");
     return 0;
 }

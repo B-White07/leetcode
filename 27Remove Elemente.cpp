1f587fcbd5002d10145a72d8f6378a960232caf0
bwhite <1240160064@qq.com>
2024-06-24 23:04:20 +0800

    update

--- 27Remove Elemente.cpp
+++ 27Remove Elemente.cpp
@@ -4,8 +4,8 @@
 //  https://leetcode.cn/problems/remove-element/description/
 /*
  * algorithm库常用函数：
- *      find
- *      remove
+ *      find()
+ *      remove()
  *
  * iterator库：使用迭代器
  */
@@ -18,4 +18,48 @@ public:
     }
 };
 
+//暴力解,超时了
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int ans = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == val) {
+                for (int j = i + 1; j < nums.size(); j++)   //从i的下一个位置开始移动元素
+                    nums[j - 1] = nums[j];  //若上一行初始化为j=i,那这里需要nums[j] = nums[j+1],这会导致j=size-1时，nums[j+1]越界
+                --i;    //此时的i指向移动后的第一个元素，这个元素还没判断是不是要删除的元素，所以要往回指一下
+                ++ans;
+            }
+        }
+        return nums.size() - ans;
+    }
+};
+
+/*
+ * 双指针，O(n)
+ */
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int left = 0, right = 0;    //right找要删除的元素，left是删除掉目标元素以后的数组的末位置
+        while (left < nums.size() && right < nums.size()) {
+            if (nums[right] != val)
+                nums[left++] = nums[right]; //初始l和r均为0，此处不会使数组变化；以后运行到这了表示找到了不等于要删除的元素的下一个元素
+                                            //left仅在删除了目标元素以后才往后走
+            ++right;    //right始终往后走
+        }
+        /*
+        while (left < nums.size() && right < nums.size()) {
+            if (nums[right] != val) {
+                nums[left] = nums[right];
+                ++left;
+                ++right;
+            } else {
+                ++right;
+            }
+        }
+         */
+        return left;
+    }
+};
 

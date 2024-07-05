4e87bb69860220cb001fd7b9741c43a97dfcf722
bwhite <1240160064@qq.com>
2024-07-05 22:11:22 +0800

    update

--- Array/3904FruitIntoBasketsM.cpp
+++ Array/3904FruitIntoBasketsM.cpp
@@ -6,9 +6,10 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-        unordered_map<int, int> mmap;
+        unordered_map<int, int> mmap;   //相当于篮子的容量为2，相同的果树只占用一个空间，用map比较合适，重复数据值保留一份。无序，使用unordered_map更高效
+                                        //unordered_map为哈希表，查找可以达到O(1).map是红黑树，时间复杂度O(nlogn)
 
-        int left = 0, ans = 0;
+        int left = 0, ans = 0;  //left指向窗口左边界，ans保存符合条件的窗口的最大长度
         for (int right = 0; right < fruits.size(); ++right) {
             ++mmap[fruits[right]];
             while (mmap.size() > 2) {

469057aab3266cc49e5bf7977ec716a070308a97
bwhite <1240160064@qq.com>
2024-07-05 22:07:07 +0800

    update

--- Array/3904FruitIntoBasketsM.cpp
+++ Array/3904FruitIntoBasketsM.cpp
@@ -2,4 +2,25 @@
  * Created by B_White on 2024/7/4.
  * https://leetcode.cn/problems/fruit-into-baskets/description/
  */
+//滑动窗口
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        unordered_map<int, int> mmap;
 
+        int left = 0, ans = 0;
+        for (int right = 0; right < fruits.size(); ++right) {
+            ++mmap[fruits[right]];
+            while (mmap.size() > 2) {
+                auto it = mmap.find(fruits[left]);
+                --it->second;
+                if (it->second == 0)
+                    mmap.erase(it);
+                ++left;
+            }
+            ans = max(right - left + 1, ans);
+        }
+
+        return ans;
+    }
+};

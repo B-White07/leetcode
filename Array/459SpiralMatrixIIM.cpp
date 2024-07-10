88e6bd52cd21a70d09295d77a2c832d5bfe57623
bwhite <1240160064@qq.com>
2024-07-10 22:04:56 +0800

    update

--- Array/459SpiralMatrixIIM.cpp
+++ Array/459SpiralMatrixIIM.cpp
@@ -31,3 +31,28 @@ public:
     }
 };
 
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n) {
+        vector<vector<int>> ans(n, vector<int>(n, 0));
+
+        int l = 0, r = n - 1, t = 0, b = n - 1;
+        int cnt = 1;
+        while (cnt <= n * n) {
+            for (int j = l; j <= r; ++j)
+                ans[t][j] = cnt++;
+            ++t;
+            for (int i = t; i <= b; ++i)
+                ans[i][r] = cnt++;
+            --r;
+            for (int j = r; j >= l; --j)
+                ans[b][j] = cnt++;
+            --b;
+            for (int i = b; i >= t; --i)
+                ans[i][l] = cnt++;
+            ++l;
+        }
+
+        return ans;
+    }
+};

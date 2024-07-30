031e85388162e8b8a8a1c7b1805890d970e1ac60
bwhite <1240160064@qq.com>
2024-07-30 21:55:09 +0800

    update

--- Hashtable/049GroupAnagramsM.cpp
+++ Hashtable/049GroupAnagramsM.cpp
@@ -5,7 +5,7 @@
 //Exceeding the time limit
 class Solution {
 public:
-    bool Anagram(string s, string t) {
+    bool Anagram(string s, string t) {  //judge if 2 string is Anagram
         if (s.size() != t.size())
             return false;
 
@@ -24,20 +24,22 @@ public:
 
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         if (strs.empty())
-            return {strs};
+            return {strs};  //return {}; is also OK.return {} can return the type the function need.
 
         vector<vector<string>> ans;
 
+        //the outer layer for loop is to group all the string.
+        //the inner layer for loop is to find which group current string belongs to.
         for (const auto& str : strs) {
-            bool found = false;
-            for (auto& group : ans) {
-                if (Anagram(str, group[0])) {
-                    group.push_back(str);
+            bool found = false; //represent if current str belongs to any group in current vector ans.
+            for (auto& group : ans) {   //traverse every group anagrams in ans.
+                if (Anagram(str, group[0])) {   //group[0] can access the first str in one group,compare this two is anagram or not
+                    group.push_back(str);   //①if they are,no need to find,the str belongs to current group
                     found = true;
                     break;
                 }
             }
-            if (!found) {
+            if (!found) {   //⓶if they are not,create a new group in the ans vector.
                 ans.push_back({str});
             }
         }
@@ -45,3 +47,26 @@ public:
     }
 };
 
+//
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        if (strs.empty())
+            return {};
+
+        unordered_map<string, vector<string>> hash; //store the a set of same Anagrams as value,the key is one ordered string of the group
+
+        for (const auto& str : strs) {  //divide different groups and save the same group as value of a certain key
+            string key = str;
+            sort(key.begin(), key.end());   //the ordered key is the real key of a group of Anagrams
+            hash[key].push_back(str);
+        }
+
+        vector<vector<string>> ans;
+        for (const auto& pair : hash) {
+            ans.push_back(pair.second); //insert every group into the vector
+        }
+
+        return ans;
+    }
+};

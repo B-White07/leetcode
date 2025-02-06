14bbfaaf8190e7ce588861a4b56a420a07917220
bwhite <1240160064@qq.com>
2025-02-06 22:00:46 +0800

    update

---
+++ Hashtable/202E.cpp
@@ -0,0 +1,29 @@
+/**
+ * Created by B_White on 2025/2/6.
+ * https://leetcode.cn/problems/happy-number/description/
+ */
+//注意题目描述，结果只有 无限循环 和 1 这两种情况，所以他一定会循环，想哈希表
+//有时间可以再想一下怎么证明必循环而不是无限不循环
+
+class Solution {
+public:
+    bool isHappy(int n) {
+        unordered_set<int> result;
+        while (1) {
+            int sum = 0;
+            while (n / 10 != 0) {
+                sum += (n % 10) * (n % 10);
+                n = n / 10;
+            }
+            sum += n * n; //最后只剩个位数的时候还需要一次处理
+            n = sum;
+
+            if (sum == 1)
+                return true;
+            else if (result.end() == result.find(sum))
+                result.insert(sum);
+            else
+                return false;
+        }
+    }
+};
